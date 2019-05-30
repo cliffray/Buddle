0ee1b2e4e8d1ca8e6b893683a5f77061b9326f5c
cliffray <long910043@gmail.com>
2019-05-30 12:23:54 +0800

    Initial commit

---
+++ app/src/main/java/com/hw/buddle/ResultActivity.java
@@ -0,0 +1,38 @@
+package com.hw.buddle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+
+public class ResultActivity extends AppCompatActivity {
+    private TextView tvResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_activity);
+        tvResult = (TextView)findViewById(R.id.tvResult);
+        showResult();
+    }
+    private void showResult(){
+        NumberFormat nf = NumberFormat.getInstance();
+        Bundle bundle = getIntent().getExtras();
+        int programming = bundle.getInt("programming");
+        int dataStructure = bundle.getInt("dataStructure");
+        int algorithm = bundle.getInt("algorithm");
+        int sum = programming + dataStructure + algorithm;
+        double average = sum/3.0;
+        String text = "programming = " + programming +
+                "\ndataStructure = " + dataStructure +
+                "\nalgorithm = " + algorithm +
+                "\nsum = " + sum +
+                "\naverage = " + nf.format(average);
+        tvResult.setText(text);
+    }
+    public void onBackClick(View view){
+        finish();
+    }
+}
